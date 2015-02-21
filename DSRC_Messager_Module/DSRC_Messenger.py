47868395fb86a8075b572d48929ee822bcf732f0
XuepengXu <xukobe@gmail.com>
2015-02-21 18:28:17 -0500

    close socket

--- DSRC_Messager_Module/DSRC_Messenger.py
+++ DSRC_Messager_Module/DSRC_Messenger.py
@@ -66,6 +66,7 @@ class SocketClient(threading.Thread):
 
     def stop_self(self):
         self.running = False
+        self.sock.shutdown()
         self.sock.close()
 
 class SocketServer(threading.Thread):
@@ -91,6 +92,7 @@ class SocketServer(threading.Thread):
 
     def stop_self(self):
         self.running = False
+        self.server_socket.close()
 
 # def main():
 #     sock = trafficsocket()
