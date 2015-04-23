693d284b3600d50a80d1fae28dacac310e6c3d4e
Xuepeng Xu <xukobe@gmail.com>
2015-04-23 15:33:24 -0400

    update VSmart.sh

--- VSmart.sh
+++ VSmart.sh
@@ -4,12 +4,13 @@ if [ $# -eq 0 ]
 then
     python /etc/DSRC/DSRC_Main/main.py
 else
-then
     if [ "$1" = "simulation" ]
         then
             python /etc/DSRC/DSRC_Main/main.py simulation
     elif [ "$1" = "stationary" ]
         then
             python /etc/DSRC/DSRC_Main/main.py stationary
+    fi
+fi
 
 
