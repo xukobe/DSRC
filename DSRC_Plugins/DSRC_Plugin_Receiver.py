e4049da69d6ab97c92e87e5716ff70c1e49c04ac
XuepengXu <xukobe@gmail.com>
2015-05-15 17:50:11 -0400

    auto setup

--- DSRC_Plugins/DSRC_Plugin_Receiver.py
+++ DSRC_Plugins/DSRC_Plugin_Receiver.py
@@ -11,6 +11,8 @@ do = False
 execute_time = 0
 time_duration = 30
 
+auto_time = 0
+
 
 def customized_event_handler(dsrc_unit, event):
     if dsrc_unit.seq == event.seq:
@@ -29,27 +31,31 @@ def customized_event_handler(dsrc_unit, event):
                 do = event.do_it
                 # print str(do)
         elif event.subtype == 'auto_setup':
-            x = event.x
-            y = event.y
-            d = event.r
-            jobs = dsrc_unit.position_tracker.jobs_to_go(x, y)
-            job1_arg = jobs['job1']
-            job2_arg = jobs['job2']
-            current_r = dsrc_unit.position_tracker.radian/(math.pi*2)*360
-            changebyjob1 = job1_arg[2] * job1_arg[1]
-            d_after_change = current_r + changebyjob1
-            diff = (d - d_after_change) % 360
-            rotate_speed = 45
-            if diff > 180:
-                diff = 360 - diff
-                rotate_speed = -45
-            job3_time = abs(diff/float(rotate_speed))
-            job1 = Job(dsrc_unit, DSRC_JobProcessor.GO, job1_arg[2], job1_arg[0], job1_arg[1])
-            job2 = Job(dsrc_unit, DSRC_JobProcessor.GO, job2_arg[2], job2_arg[0], job2_arg[1])
-            job3 = Job(dsrc_unit, DSRC_JobProcessor.GO, job3_time, 0, rotate_speed)
-            dsrc_unit.job_processor.add_new_job(job1)
-            dsrc_unit.job_processor.add_new_job(job2)
-            dsrc_unit.job_processor.add_new_job(job3)
+            current_time = time.time()
+            global auto_time
+            if current_time - auto_time > 30:
+                x = event.x
+                y = event.y
+                d = event.r
+                jobs = dsrc_unit.position_tracker.jobs_to_go(x, y)
+                job1_arg = jobs['job1']
+                job2_arg = jobs['job2']
+                current_r = dsrc_unit.position_tracker.radian/(math.pi*2)*360
+                changebyjob1 = job1_arg[2] * job1_arg[1]
+                d_after_change = current_r + changebyjob1
+                diff = (d - d_after_change) % 360
+                rotate_speed = 45
+                if diff > 180:
+                    diff = 360 - diff
+                    rotate_speed = -45
+                job3_time = abs(diff/float(rotate_speed))
+                job1 = Job(dsrc_unit, DSRC_JobProcessor.GO, job1_arg[2], job1_arg[0], job1_arg[1])
+                job2 = Job(dsrc_unit, DSRC_JobProcessor.GO, job2_arg[2], job2_arg[0], job2_arg[1])
+                job3 = Job(dsrc_unit, DSRC_JobProcessor.GO, job3_time, 0, rotate_speed)
+                dsrc_unit.job_processor.add_new_job(job1)
+                dsrc_unit.job_processor.add_new_job(job2)
+                dsrc_unit.job_processor.add_new_job(job3)
+                auto_time = time.time()
 
 
 def print_receiver():
